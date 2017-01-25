d1b96c5abf145af99235dfbc49783b4652f963b3
Thomas-R-georgiancollege <200343345@student.georgianc.on.ca>
2017-01-25 14:08:58 -0500

    hourly employee with couple error

--- src/com/company/HourlyEmployee.java
+++ src/com/company/HourlyEmployee.java
@@ -6,13 +6,40 @@ import java.time.LocalDate;
  * Created by Thomas on 2017-01-25.
  */
 public class HourlyEmployee extends Employee {
+    private double minimumWage = 11.40;
+    private double hourlyRate, hoursWorked;
 
     public HourlyEmployee(String first, String last, int sin, LocalDate dob) {
         super(first, last, sin, dob);
     }
+    hoursWorked=0;
 
+    setPayRate(payRate);
+
+    //this method will validate that the pay rate is above minimum wage and set the instance variable
+
+    public void setPayRate(double payRate)
+    {
+        if (payRate >= minimumWage)
+            this.hourlyRate = payRate;
+        else
+            throw new IllegalArgumentException("hey cheapo - minimum wage is " + minimumWage);
+    }
+
+    //this method will add hours worked to the employees history
+    public void addHoursWorked(double hoursWorked)
+    {
+        if (hoursWorked > 0)
+            this.hoursWorked += hoursWorked;
+        else
+            throw new IllegalArgumentException("new hours worked must be greater than 0");
+    }
     @Override
     public PayCheque getPayCheque() {
-        return null;
+        double payAmount = hoursWorked * hourlyRate;
+        PayCheque newPayCheque = new PayCheque(super.toString(), payAmount, "Your pay cheque");
+
+        hoursWorked =0;
+        return newPayCheque;
     }
 }

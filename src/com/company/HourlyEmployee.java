a1d5fcabe5d188e00a2c9e1030b0501c67a84e95
Thomas-R-georgiancollege <200343345@student.georgianc.on.ca>
2017-01-25 13:53:54 -0500

    Adding basic info

--- src/com/company/HourlyEmployee.java
+++ src/com/company/HourlyEmployee.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import java.time.LocalDate;
+
 /**
  * Created by Thomas on 2017-01-25.
  */
-public class HourlyEmployee {
+public class HourlyEmployee extends Employee {
 
+    public HourlyEmployee(String first, String last, int sin, LocalDate dob) {
+        super(first, last, sin, dob);
+    }
+
+    @Override
+    public PayCheque getPayCheque() {
+        return null;
+    }
 }

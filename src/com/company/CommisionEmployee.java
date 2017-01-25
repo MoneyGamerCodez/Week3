898c61360c3aa9a66036f75d47362fef6420f2bb
Thomas-R-georgiancollege <200343345@student.georgianc.on.ca>
2017-01-25 14:54:43 -0500

    requires some work

--- src/com/company/CommisionEmployee.java
+++ src/com/company/CommisionEmployee.java
@@ -8,9 +8,8 @@ import java.time.LocalDate;
 public class CommissionEmployee extends Employee {
     private double monthlySales, commisionRate;
 
-    public CommisionEmployee(String first, String last, int sin, LocalDate dob) {
-        super(first, last, sin, dob, double commRate);
-
+    public CommissionEmployee(String first, String last, int sin, LocalDate dob,double commRate) {
+        super(first, last, sin, dob);
         monthlySales = 0;
 
         setCommRate(commRate);
@@ -18,7 +17,7 @@ public class CommissionEmployee extends Employee {
 
     public void setCommRate(double commRate)
     {
-        if (commRate > 0 && < 60)
+        if (commRate > 0 && commRate < 60)
             commisionRate = commRate;
         else
             throw new IllegalArgumentException("Commissions must be set between 0 - 60");
@@ -35,6 +34,6 @@ public class CommissionEmployee extends Employee {
     public PayCheque getPayCheque() {
         double payAmount = commisionRate/100 * monthlySales;
 
-        PayCheque newPay = new PayCheque(super.toString(), payAmount, "commission pay")
+        PayCheque newPay = new PayCheque(super.toString(), payAmount, "commission pay");
     }
 }

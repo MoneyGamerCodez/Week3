7635da3d97b2a9cef7113a58fb0b41cecf4faf51
Thomas-R-georgiancollege <200343345@student.georgianc.on.ca>
2017-01-25 13:16:40 -0500

    constructor

--- src/com/company/PayCheque.java
+++ src/com/company/PayCheque.java
@@ -1,7 +1,23 @@
 package com.company;
 
+import java.time.LocalDate;
+
 /**
  * Created by Thomas on 2017-01-25.
  */
 public class PayCheque {
+    private LocalDate dateIssued;
+    private String employeeName;
+    private double amount;
+    private String memo;
+
+    public PayCheque(LocalDate dateIssued, String employeeName, double amount, String memo) {
+        this.dateIssued = dateIssued;
+        this.employeeName = employeeName;
+        this.amount = amount;
+        this.memo = memo;
+    }
+
+
+
 }

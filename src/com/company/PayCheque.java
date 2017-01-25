076f9a6844791241329cbdaff7063a7522d63dc5
Thomas-R-georgiancollege <200343345@student.georgianc.on.ca>
2017-01-25 13:20:47 -0500

    updated constructor

--- src/com/company/PayCheque.java
+++ src/com/company/PayCheque.java
@@ -10,12 +10,17 @@ public class PayCheque {
     private String employeeName;
     private double amount;
     private String memo;
+    private int chequeNumber;
+    private static int nextChequeNumber = 10001;
 
     public PayCheque(LocalDate dateIssued, String employeeName, double amount, String memo) {
         this.dateIssued = dateIssued;
         this.employeeName = employeeName;
         this.amount = amount;
         this.memo = memo;
+        chequeNumber = nextChequeNumber;
+
+        nextChequeNumber++;
     }
 
 
